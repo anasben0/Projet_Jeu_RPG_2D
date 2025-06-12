58ffd3e1d39bddec92d5e1d1150c7ae992693b35
Valisoa Randrianoelina <valisoa.randrianoelina@universite-paris-saclay.fr>
2025-06-12 20:24:07 +0000

    Delete Equipements.java

--- src/model/gameplay/Equipements.java
+++
@@ -1,7 +0,0 @@
-package model.gameplay;
-import java.util.List;
-import model.item.Item;
-    // Classe pour regroupement des équipements
-public abstract class Equipements {
-    public List<Item> items;
-}
